93938a4cc43b596c0233c9db9fb2585f5bcce17b
YI ZHANG <scaccterran@gmail.com>
2021-06-04 21:53:51 -0400

    FOC-6 work item management component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,38 @@
 <template>
-    <div></div>
+    <work-items-management class="work-items-management"></work-items-management>
 </template>
 
 <script lang="ts">
-import { Vue } from 'vue-class-component';
+import { Options, Vue } from 'vue-class-component';
 
+import WorkItemsManagement from './features/work-items-management/WorkItemsManagement.vue';
+
+@Options({
+    components: {
+        WorkItemsManagement
+    }
+})
 export default class App extends Vue {}
 </script>
 
 <style lang="scss">
+html, body, #app {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    width: 100%;
+    height: 100%;
+    user-select: none;
+    background-color: transparent;
+}
+
+#app {
+    display: flex;
+    flex-direction: column;
+}
+
+.work-items-management {
+    width: 100%;
+    height: 75%;
+}
 </style>
---
+++ src/features/work-items-management/WorkItemsManagement.vue
@@ -0,0 +1,17 @@
+<template>
+    <div></div>
+</template>
+
+<script lang="ts">
+import { Vue } from 'vue-class-component';
+
+import store from '../../store';
+import { workItemKey } from '../../store/work-item/work-item.state';
+
+export default class WorkItemsManagement extends Vue {
+
+    public created(): void {
+        store.dispatch(`${workItemKey}/loadWorkItems`);
+    }
+}
+</script>
