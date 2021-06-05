4620874f3180e0e20daaccdf7d82b982eb72e856
YI ZHANG <scaccterran@gmail.com>
2021-06-05 18:19:35 -0400

    FOC-10 extracted main background

--- src/app.vue
+++ src/app.vue
@@ -1,16 +1,5 @@
 <template>
-    <div class="background-view">
-        <div class="glare"></div>
-
-        <div class="grids">
-            <div class="grid-row" v-for="row in 12" :key="row">
-                <div class="grid" v-for="column in 22" :key="column">
-                    <div></div>
-                    <div></div>
-                </div>
-            </div>
-        </div>
-    </div>
+    <lightsource-panel class="lightsource-panel"></lightsource-panel>
 
     <div class="header-displays">
         <current-date-time></current-date-time>
@@ -25,12 +14,14 @@
 import { Options, Vue } from 'vue-class-component';
 
 import WorkItemsManagement from './features/work-items-management/work-items-management.vue';
+import LightsourcePanel from './shared/panels/lightsource-panel.vue';
 import ContentViewPanel from './shared/panels/content-view-panel.vue';
 import CurrentDateTime from './shared/widgets/current-date-time.vue';
 
 @Options({
     components: {
         WorkItemsManagement,
+        LightsourcePanel,
         ContentViewPanel,
         CurrentDateTime
     }
@@ -62,71 +53,10 @@ html, body, #app {
     font-size: 16px;
 }
 
-.background-view {
+.lightsource-panel {
     position: absolute;
     top: 0;
     left: 0;
-    width: 100%;
-    height: 100%;
-    overflow: hidden;
-
-    .glare {
-        position: absolute;
-        top: -70vh;
-        left: -5vw;
-        width: 110vw;
-        height: 150vh;
-        background: radial-gradient(
-            57.36% 57.36% at 50% 42.64%,
-            rgba(255, 255, 255, 0.125) 0%,
-            rgba(248, 248, 248, 0.125) 0.01%,
-            rgba(138, 138, 138, 0) 100%
-        );
-    }
-
-    .grids {
-        position: absolute;
-        top: 0;
-        left: 0;
-        display: flex;
-        flex-direction: column;
-        justify-content: space-evenly;
-        width: 100%;
-        height: 100%;
-
-        .grid-row {
-            display: flex;
-            justify-content: space-evenly;
-        }
-
-        .grid {
-            $grid-line-width: 1px;
-            $grid-dimension: 1.5vh;
-
-            position: relative;
-            width: $grid-dimension;
-            height: $grid-dimension;
-
-            & > div {
-                position: absolute;
-                background-color: var(--primary-colors-601);
-
-                &:first-of-type {
-                    top: 0;
-                    left: calc(50% - #{$grid-line-width} / 2);
-                    width: $grid-line-width;
-                    height: $grid-dimension;
-                }
-
-                &:last-of-type {
-                    top: calc(50% - #{$grid-line-width} / 2);
-                    left: 0;
-                    width: $grid-dimension;
-                    height: $grid-line-width;
-                }
-            }
-        }
-    }
 }
 
 .header-displays {
