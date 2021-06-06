8029a5c3913cb38089d3d2636d7935e92d9e3fe4
YI ZHANG <scaccterran@gmail.com>
2021-06-06 10:08:32 -0400

    FOC-15 added status selection menu

---
+++ src/features/work-items-management/work-items-list/work-item-status-menu/work-item-status-menu.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="work-item-status-menu-container">
+        <checkbox-marked-circle-outline class="icon" />
+        <play-circle class="icon" />
+        <target class="icon" />
+    </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import { CheckboxMarkedCircleOutline, PlayCircle, Target } from 'mdue';
+
+@Options({
+    components: {
+        CheckboxMarkedCircleOutline,
+        PlayCircle,
+        Target
+    }
+})
+export default class WorkItemStatusMenu extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+.work-item-status-menu-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-around;
+
+    .icon {
+        color: var(--font-colors-400);
+        font-size: var(--font-sizes-400);
+        transition: font-size 0.1s, color 0.3s;
+
+        &:hover {
+            cursor: pointer;
+            color: var(--font-colors-000);
+            font-size: var(--font-sizes-600);
+        }
+    }
+}
+</style>
