fadb40c89472f350a8799f0c11cec13bf9a8e8d5
YI ZHANG <scaccterran@gmail.com>
2021-06-07 16:22:52 -0400

    FOC-12 added daily focus progression display

--- src/app.vue
+++ src/app.vue
@@ -3,6 +3,7 @@
 
     <div class="header-displays">
         <current-date-time></current-date-time>
+        <daily-focus-progression class="daily-progression"></daily-focus-progression>
     </div>
 
     <work-items-management class="work-items-management"></work-items-management>
@@ -16,12 +17,14 @@ import { performanceKey } from './store/performance/performance.state';
 import WorkItemsManagement from './features/work-items-management/work-items-management.vue';
 import LightsourcePanel from './shared/panels/lightsource-panel.vue';
 import CurrentDateTime from './shared/widgets/current-date-time.vue';
+import DailyFocusProgression from './shared/widgets/daily-focus-progression.vue';
 
 @Options({
     components: {
         WorkItemsManagement,
         LightsourcePanel,
-        CurrentDateTime
+        CurrentDateTime,
+        DailyFocusProgression
     }
 })
 export default class App extends Vue {
@@ -63,11 +66,17 @@ html, body, #app {
 }
 
 .header-displays {
+    position: relative;
     display: flex;
     justify-content: space-around;
     align-items: center;
     width: 100%;
     height: $header-displays-height;
+
+    .daily-progression {
+        position: absolute;
+        right: 5vh;
+    }
 }
 
 .work-items-management {
