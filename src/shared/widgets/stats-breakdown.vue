abe71bc7682befcfd5f9eb69813fefead4f42465
YI ZHANG <scaccterran@gmail.com>
2021-06-07 21:42:16 -0400

    FOC-18 stats breakdown component

---
+++ src/shared/widgets/stats-breakdown.vue
@@ -0,0 +1,33 @@
+<template>
+    <display-panel class="stats-breakdown-container">
+        <span>{{ title }}</span>
+        <span>{{ content }}</span>
+        <progress-bar :series="series"></progress-bar>
+    </display-panel>
+</template>
+
+<script lang="ts">
+import { Options, Vue, prop } from 'vue-class-component';
+
+import { PercentageSeries } from '../../core/models/progress-bar/percentage-series';
+import DisplayPanel from '../panels/display-panel.vue';
+import ProgressBar from '../displays/progress-bar.vue';
+
+class StatsBreakdownProp {
+    public title = prop<string>({ default: '' });
+    public content = prop<string>({ default: '' });
+    public series = prop<PercentageSeries[]>({ default: [] });
+}
+
+@Options({
+    components: {
+        DisplayPanel,
+        ProgressBar
+    }
+})
+export default class StatsBreakdown extends Vue.with(StatsBreakdownProp) {}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
