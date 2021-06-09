0ccf91d359871fc0f31b65fff17f50019543c9df
YI ZHANG <scaccterran@gmail.com>
2021-06-09 07:24:39 -0400

    FOC-17 task radar component

---
+++ src/shared/widgets/task-radar.vue
@@ -0,0 +1,87 @@
+<template>
+    <display-panel class="task-radar-container" :lineLength="'1vh'">
+        <div class="rings">
+            <div class="ring" v-for="n in 5" :key="n"></div>
+            <div class="grid-line" v-for="n in 2" :key="n"></div>
+        </div>
+    </display-panel>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+import DisplayPanel from '../panels/display-panel.vue';
+
+@Options({
+    components: { DisplayPanel }
+})
+export default class TaskRadar extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+.task-radar-container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: var(--primary-colors-8-01);
+
+    .rings {
+        position: relative;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 22.5vh;
+        height: 22.5vh;
+        border-radius: 50%;
+        background-color: var(--primary-colors-0-01);
+
+        .ring {
+            $ring-width: calc(100% / 6);
+
+            position: absolute;
+            border-radius: 50%;
+            border: 1px solid var(--primary-colors-0-03);
+
+            &:nth-child(1) {
+                width: $ring-width;
+                height: $ring-width;
+            }
+
+            &:nth-child(2) {
+                width: calc(#{$ring-width} * 2);
+                height: calc(#{$ring-width} * 2);
+            }
+
+            &:nth-child(3) {
+                width: calc(#{$ring-width} * 3);
+                height: calc(#{$ring-width} * 3);
+            }
+
+            &:nth-child(4) {
+                width: calc(#{$ring-width} * 4);
+                height: calc(#{$ring-width} * 4);
+            }
+
+            &:nth-child(5) {
+                width: calc(#{$ring-width} * 5);
+                height: calc(#{$ring-width} * 5);
+            }
+        }
+
+        .grid-line {
+            position: absolute;
+            background-color: var(--primary-colors-0-03);
+
+            &:nth-child(6) {
+                width: 100%;
+                height: 1px;
+            }
+
+            &:nth-child(7) {
+                width: 1px;
+                height: 100%;
+            }
+        }
+    }
+}
+</style>
