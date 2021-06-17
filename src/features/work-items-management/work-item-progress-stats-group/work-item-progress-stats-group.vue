539401b2fd32b5cebf73f3578cb5878999816f13
YI ZHANG <scaccterran@gmail.com>
2021-06-17 19:47:40 -0400

    FOC-25 added average daily focus breakdown

--- src/features/work-items-management/work-item-progress-stats-group/work-item-progress-stats-group.vue
+++ src/features/work-items-management/work-item-progress-stats-group/work-item-progress-stats-group.vue
@@ -6,6 +6,13 @@
             :content="pastDueAndLooming"
             :series="pastDueAndLoomingSeries">
         </stats-breakdown>
+
+        <stats-breakdown v-if="activityHistories"
+            class="breakdown"
+            :title="'average daily focus'"
+            :content="averageFocus"
+            :series="dailyFocusSeries">
+        </stats-breakdown>
     </div>
 </template>
 
@@ -15,7 +22,9 @@ import { Options, Vue } from 'vue-class-component';
 import store from '../../../store';
 import { performanceKey } from '../../../store/performance/performance.state';
 import { DueDateBreakdownDto } from '../../../core/dtos/due-date-breakdown-dto';
+import { ActivityBreakdownDto } from '../../../core/dtos/activity-breakdown-dto';
 import { PercentageSeries } from '../../../core/models/progress-bar/percentage-series';
+import { GenericUtility } from '../../../core/utilities/generic-utility/generic-utility';
 import StatsBreakdown from '../../../shared/widgets/stats-breakdown.vue';
 
 @Options({
@@ -42,6 +51,42 @@ export default class WorkItemProgressStatsGroup extends Vue {
         return [
             { percent: pastDue / total * 100, colorType: 'context-colors-warning' },
             { percent: looming / total * 100, colorType: 'context-colors-alert' }
+        ];
+    }
+
+    get activityHistories(): ActivityBreakdownDto[] {
+        return store.getters[`${performanceKey}/activityHistories`] ?? [];
+    }
+
+    get averageFocus(): string {
+        const hours = this.activityHistories.reduce((total, _) => total + _.regular + _.recurring + _.overlearning, 0);
+        const average = GenericUtility.roundTo(hours / this.activityHistories.length, 1);
+
+        return `${average} hour${average > 1 ? 's' : ''}`;
+    }
+
+    get dailyFocusSeries(): PercentageSeries[] {
+        const total = this.activityHistories.length;
+        const aggregate = { insufficient: 0, sufficient: 0, overdoing: 0 };
+
+        const { insufficient, sufficient, overdoing } = this.activityHistories.reduce((_, history) => {
+            const focus = history.regular + history.recurring + history.overlearning;
+
+            if (focus < 8) {
+                return { ..._, insufficient: _.insufficient + 1 };
+            }
+
+            if (focus < 12) {
+                return { ..._, sufficient: _.sufficient + 1 };
+            }
+
+            return { ..._, overdoing: _.overdoing + 1 };
+        }, aggregate);
+
+        return [
+            { percent: overdoing / total * 100, colorType: 'focus-progress-colors-overdoing' },
+            { percent: insufficient / total * 100, colorType: 'focus-progress-colors-insufficient' },
+            { percent: sufficient / total * 100, colorType: 'focus-progress-colors-sufficient' }
         ];
     }
 }
