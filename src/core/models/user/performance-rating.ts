b970eaae727b6fe12667d5ffd9d7c77d97130d37
YI ZHANG <scaccterran@gmail.com>
2021-06-18 19:21:52 -0400

    FOC-34 allow getting user profile

---
+++ src/core/models/user/performance-rating.ts
@@ -0,0 +1,7 @@
+export class PerformanceRating {
+    public determination = 50;
+    public estimation = 50;
+    public planning = 50;
+    public adaptability = 50;
+    public sustainability = 50;
+}
