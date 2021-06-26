1275b1dbee5de52bd2abae8a0055a202bcec7c0f
YI ZHANG <scaccterran@gmail.com>
2021-06-26 18:02:48 -0400

    FOC-37 global test setup

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,12 @@
 module.exports = {
     preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
     transform: { '^.+\\.vue$': 'vue-jest' },
+    setupFilesAfterEnv: ['<rootDir>/src/specs.ts'],
+    testMatch: ['**/src/**/*.spec.ts'],
     collectCoverage: true,
+    coverageReporters: ['cobertura', 'text', 'text-summary'],
     collectCoverageFrom: [
-        '**src/**/*.{js,vue}',
-        '!**/node_modules/**'
+        '**/src/**/*.{js,ts,vue}',
+        '!**/{node_modules,mocks}/**'
     ]
-}
+};
