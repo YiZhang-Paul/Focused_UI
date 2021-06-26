512825cc993a699e04044b8c23bb89766ba2573d
YI ZHANG <scaccterran@gmail.com>
2021-06-26 17:22:11 -0400

    FOC-40 setup inversify

---
+++ src/core/ioc/types.ts
@@ -0,0 +1,6 @@
+export const types = {
+    UserProfileHttpService: Symbol.for('UserProfileHttpService'),
+    TimeSessionHttpService: Symbol.for('TimeSessionHttpService'),
+    WorkItemHttpService: Symbol.for('WorkItemHttpService'),
+    PerformanceHttpService: Symbol.for('PerformanceHttpService'),
+}
