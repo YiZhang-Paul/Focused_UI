6d43754c857a439ab6be1d08dd055afee680fdb8
YI ZHANG <scaccterran@gmail.com>
2021-07-04 13:49:33 -0400

    FOC-37 fixed broken tests

--- src/features/work-items-management/work-items-management.spec.ts
+++ src/features/work-items-management/work-items-management.spec.ts
@@ -9,7 +9,6 @@ import { container } from '../../core/ioc/container';
 import { FocusSessionDto } from '../../core/dtos/focus-session-dto';
 import { WorkItemDto } from '../../core/dtos/work-item-dto';
 import { WorkItem } from '../../core/models/work-item/work-item';
-import { WorkItemType } from '../../core/enums/work-item-type.enum';
 import { WorkItemHttpService } from '../../core/services/http/work-item-http/work-item-http.service';
 
 import WorkItemsManagement from './work-items-management.vue';
@@ -178,131 +177,6 @@ describe('work items management unit test', () => {
             await component.vm.onItemStop();
 
             sinonExpect.calledOnce(workItemHttpStub.stopWorkItem);
-        });
-    });
-
-    describe('onSearch', () => {
-        test('should load items', () => {
-            component.vm.onSearch('search_text');
-
-            sinonExpect.calledOnce(workItemHttpStub.getWorkItems);
-            expect(workItemHttpStub.getWorkItems.getCall(0).args[0].searchText).toEqual('search_text');
-        });
-    });
-
-    describe('onCompletionFilter', () => {
-        test('should set query properly when no filter is applied', () => {
-            component.vm.completionFilterOptions[0].isActive = true;
-            component.vm.completionFilterOptions[1].isActive = false;
-            component.vm.completionFilterOptions[2].isActive = false;
-
-            component.vm.onCompletionFilter();
-
-            sinonExpect.calledOnce(workItemHttpStub.getWorkItems);
-            expect(workItemHttpStub.getWorkItems.getCall(0).args[0].isCompleted).toBeUndefined();
-        });
-
-        test('should set query properly when completed filter is applied', () => {
-            component.vm.completionFilterOptions[0].isActive = false;
-            component.vm.completionFilterOptions[1].isActive = true;
-            component.vm.completionFilterOptions[2].isActive = false;
-
-            component.vm.onCompletionFilter();
-
-            sinonExpect.calledOnce(workItemHttpStub.getWorkItems);
-            expect(workItemHttpStub.getWorkItems.getCall(0).args[0].isCompleted).toEqual(true);
-        });
-
-        test('should reset highlight filter when completed filter is applied', () => {
-            component.vm.highlightFilterOptions[0].isActive = false;
-            component.vm.highlightFilterOptions[1].isActive = true;
-            component.vm.highlightFilterOptions[2].isActive = false;
-            component.vm.completionFilterOptions[0].isActive = false;
-            component.vm.completionFilterOptions[1].isActive = true;
-            component.vm.completionFilterOptions[2].isActive = false;
-
-            component.vm.onCompletionFilter();
-
-            expect(workItemHttpStub.getWorkItems.getCall(0).args[0].isHighlighted).toBeUndefined();
-            expect(component.vm.highlightFilterOptions[0].isActive).toBeTruthy();
-            expect(component.vm.highlightFilterOptions[1].isActive).toBeFalsy();
-            expect(component.vm.highlightFilterOptions[2].isActive).toBeFalsy();
-        });
-
-        test('should set query properly when incomplete filter is applied', () => {
-            component.vm.completionFilterOptions[0].isActive = false;
-            component.vm.completionFilterOptions[1].isActive = false;
-            component.vm.completionFilterOptions[2].isActive = true;
-
-            component.vm.onCompletionFilter();
-
-            sinonExpect.calledOnce(workItemHttpStub.getWorkItems);
-            expect(workItemHttpStub.getWorkItems.getCall(0).args[0].isCompleted).toEqual(false);
-        });
-    });
-
-    describe('onHighlightFilter', () => {
-        test('should set query properly when no filter is applied', () => {
-            component.vm.highlightFilterOptions[0].isActive = true;
-            component.vm.highlightFilterOptions[1].isActive = false;
-            component.vm.highlightFilterOptions[2].isActive = false;
-
-            component.vm.onHighlightFilter();
-
-            sinonExpect.calledOnce(workItemHttpStub.getWorkItems);
-            expect(workItemHttpStub.getWorkItems.getCall(0).args[0].isHighlighted).toBeUndefined();
-        });
-
-        test('should set query properly when highlighted filter is applied', () => {
-            component.vm.highlightFilterOptions[0].isActive = false;
-            component.vm.highlightFilterOptions[1].isActive = true;
-            component.vm.highlightFilterOptions[2].isActive = false;
-
-            component.vm.onHighlightFilter();
-
-            sinonExpect.calledOnce(workItemHttpStub.getWorkItems);
-            expect(workItemHttpStub.getWorkItems.getCall(0).args[0].isHighlighted).toEqual(true);
-        });
-
-        test('should reset completion filter when highlighted filter is applied', () => {
-            component.vm.completionFilterOptions[0].isActive = false;
-            component.vm.completionFilterOptions[1].isActive = true;
-            component.vm.completionFilterOptions[2].isActive = false;
-            component.vm.highlightFilterOptions[0].isActive = false;
-            component.vm.highlightFilterOptions[1].isActive = true;
-            component.vm.highlightFilterOptions[2].isActive = false;
-
-            component.vm.onHighlightFilter();
-
-            expect(workItemHttpStub.getWorkItems.getCall(0).args[0].isCompleted).toBeUndefined();
-            expect(component.vm.completionFilterOptions[0].isActive).toBeTruthy();
-            expect(component.vm.completionFilterOptions[1].isActive).toBeFalsy();
-            expect(component.vm.completionFilterOptions[2].isActive).toBeFalsy();
-        });
-
-        test('should set query properly when not highlighted filter is applied', () => {
-            component.vm.highlightFilterOptions[0].isActive = false;
-            component.vm.highlightFilterOptions[1].isActive = false;
-            component.vm.highlightFilterOptions[2].isActive = true;
-
-            component.vm.onHighlightFilter();
-
-            sinonExpect.calledOnce(workItemHttpStub.getWorkItems);
-            expect(workItemHttpStub.getWorkItems.getCall(0).args[0].isHighlighted).toEqual(false);
-        });
-    });
-
-    describe('onTypeFilter', () => {
-        test('should set query with proper type', () => {
-            component.vm.typeFilterOptions[0].isActive = false;
-            component.vm.typeFilterOptions[1].isActive = false;
-            component.vm.typeFilterOptions[2].isActive = true;
-            component.vm.typeFilterOptions[3].isActive = false;
-
-            component.vm.onTypeFilter();
-
-            sinonExpect.calledOnce(workItemHttpStub.getWorkItems);
-            expect(workItemHttpStub.getWorkItems.getCall(0).args[0].type).toEqual(WorkItemType.Recurring);
         });
     });
 });
